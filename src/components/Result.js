aabefe3723a7fa8a03b2c35afcfc665db6984e9d
melihyelman <myelman17@gmail.com>
2022-01-24 20:08:50 +0300

    fix result problems

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,12 @@
 
-function Result({ setQueue, correctAnswerCount }) {
+function Result({ setSelectionSection, setCorrectAnswerCount, setQueue, correctAnswerCount }) {
     return <div className='result'>
         <img src='/result.svg' alt='result.svg' />
         <div>
             <h3>Results</h3>
             <p>You got <span>{correctAnswerCount} </span>correct answers</p>
         </div>
-        <button onClick={() => setQueue(0)}>Try Again</button>
+        <button onClick={() => { setSelectionSection(true); setCorrectAnswerCount(0); setQueue(0) }}>Try Again</button>
     </div>;
 }
 
